5eaeff7d013d226b50c4866848503457516f7b11
LordofUniverse <63595155+LordofUniverse@users.noreply.github.com>
2021-07-02 23:01:49 +0530

    Update Update.js

--- src/UpdateProfile/Update.js
+++ src/UpdateProfile/Update.js
@@ -311,6 +311,13 @@ const Update = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
+    
+              <Nav.Item>
+                <Nav.Link>
+                  <Link to="/home">Home</Link>
+                </Nav.Link>
+              </Nav.Item>
+    
               <Nav.Item>
                 <Nav.Link>
                   <Link to="/vaccinationlist">Vaccination List</Link>
